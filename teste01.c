455e7b73c2e0bc44abd373b775f37a772f36b132
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-12-23 20:34:45 -0300

    Initial commit

---
+++ teste01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int idade;
+    printf("\n=============== SEJA BEM VINDO! ==============\n");
+    printf("\nQual sua idade: ");
+    scanf("%d", &idade);
+    printf("\nIdade: %d", idade);
+    printf("\n=================== FIM! =====================\n");
+
+    return 0;
+}
